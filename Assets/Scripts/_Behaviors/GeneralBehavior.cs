071ed384c480c7e4f84d32123675e84f4deb5d0a
PC-FIXE-LUCAS\fouca <foucault.lucas@gmail.com>
2016-10-24 00:30:37 +0200

    [MERGE] Merge all scripts

--- Assets/Scripts/_Behaviors/GeneralBehavior.cs
+++ Assets/Scripts/_Behaviors/GeneralBehavior.cs
@@ -1,23 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-<<<<<<< HEAD
 public class GeneralBehavior {
-=======
-public class GeneralBehavior : MonoBehaviour {
-    
-    public float _masse;
-    public float _maxSpeed;
-    public float _maxSteeringForce;
-    public Vector2 _position;
-	public Vector2 _direction;
-	public Vector2 _velocity;
-	public Vector2 _steering;
->>>>>>> d258e6e0d12ff2315ebcdbe1af461b381674093a
 
     protected Unit _unit;
-
-<<<<<<< HEAD
+    
     public GeneralBehavior(Unit unit)
     {
         _unit = unit;
@@ -28,14 +15,6 @@ public class GeneralBehavior : MonoBehaviour {
         _unit._steering = Vector2.ClampMagnitude(steering, _unit._maxSteeringForce); // steering return the force corresponding at the behavior send by param
         Vector2 acceleration = _unit._steering / _unit._masse;
         _unit._velocity = Vector2.ClampMagnitude(_unit._velocity + acceleration /* *Time.deltaTime*/, _unit._maxSpeed);
-=======
-	public Vector2 computeNewPosition(Vector2 steering)
-    {
-		_steering = Vector2.ClampMagnitude(steering, _maxSteeringForce); // steering return the force corresponding at the behavior send by param
-		//_steering.y = Vector3.ClampMagnitude(steering, _maxSteeringForce).z; // steering return the force corresponding at the behavior send by param
-		Vector2 acceleration = _steering / _masse;
-		_velocity = Vector2.ClampMagnitude(_velocity + acceleration /* *Time.deltaTime*/, _maxSpeed);
->>>>>>> d258e6e0d12ff2315ebcdbe1af461b381674093a
         
 		return _unit._position + _unit._velocity * Time.deltaTime;
     }
--- Assets/Scripts/_Units/FoxUnit.cs
+++ Assets/Scripts/_Units/FoxUnit.cs
@@ -16,28 +16,7 @@ public class FoxUnit : Unit
 
         Vector2 steering = ((SeekBehavior)_behaviors[0]).computeSeekSteering(targetPosition);
         _position = ((SeekBehavior)_behaviors[0]).computeNewPosition(steering);
-
-<<<<<<< HEAD
+        
         this.updatePosition(_position);
     }   
-=======
-	// Use this for initialization
-	void Start () {
-		_position = new Vector2 (this.transform.position.x,this.transform.position.z);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(stateSteering == 0)
-			_position = computeNewPosition(computeSeekSteering(_target)-computeSeparationSteering());
-		if(stateSteering == 1)
-			_position = computeNewPosition(computeFleeSteering(_target)-computeSeparationSteering());
-		if(stateSteering == 2)
-			_position = computeNewPosition(computePursuitSteering(_target)-computeSeparationSteering());
-		if(stateSteering == 3)
-			_position = computeNewPosition(computeEvasionSteering(_target)-computeSeparationSteering());
-		
-		this.transform.position = new Vector3(_position.x,0.0f,_position.y);	
-	}
->>>>>>> d258e6e0d12ff2315ebcdbe1af461b381674093a
 }

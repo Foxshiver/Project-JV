56edf7b6847e030947340e14d5030b42f43b00e7
Vincent Goupil <goupil.vince@gmail.com>
2016-10-24 16:56:25 +0200

    [LeaderBehavior] Add getBehindLeader()

--- Assets/Scripts/_Behaviors/LeaderBehavior.cs
+++ Assets/Scripts/_Behaviors/LeaderBehavior.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class LeaderBehavior : GeneralBehavior
 {
     public float _radius;
+    private int LEADER_BEHIND_DIST = 5;
 
     public LeaderBehavior(Unit unit) : base(unit)
     {
@@ -42,6 +43,18 @@ public class LeaderBehavior : GeneralBehavior
     // Functions
     public float getRadius() {
         return _radius;
+    }
+
+    public Vector2 getBehindLeader()
+    {
+        Vector2 behind;
+
+        Vector2 tv = _unit._velocity * -1;
+        tv = tv * LEADER_BEHIND_DIST;
+
+        behind = _unit._position + tv;
+
+        return behind;
     }
 
 }

596441f9cb892b030007ce1ab43b5cf96e9af7b7
Vincent Goupil <goupil.vince@gmail.com>
2017-01-17 18:16:38 +0100

    [Models] Fox Rigged

---
+++ Assets/Scripts/MoveNavMeshAgent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveNavMeshAgent : MonoBehaviour {
+
+    NavMeshAgent agent;
+    public NavMeshAgent target;
+	// Use this for initialization
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+        //agent.SetDestination(target.nextPosition);
+        agent.Move(new Vector2(3 * Time.deltaTime, 0));
+	}
+}

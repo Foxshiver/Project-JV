13d5da2c7cc3de39028afc4cb3a4e8cfecb396e8
Martin Toutirais <martin.toutirais@outlook.fr>
2017-02-06 12:17:01 +0100

    [End Scene]

---
+++ Assets/Scripts/PersistentData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersistentData : MonoBehaviour
+{
+    public static float TimeAlive;
+    public static int nbOfWaves;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
